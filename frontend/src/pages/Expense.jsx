88377bea54f422cfe27cf38c8e8ccb21669ce698
dominiclet <dominic.let@hotmail.com>
2021-11-11 14:42:02 +0800

    Add edit individual expense page

--- frontend/src/pages/Expense.jsx
+++ frontend/src/pages/Expense.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { categories } from "../lib/config";
+import styles from "../styles/IndividualExpense.module.css";
 
 const Expense = () => {
 
@@ -17,9 +20,54 @@ const Expense = () => {
         updated_by: "Jacky"
     }
 
+	const [description, setDescription] = useState(expenseInfo.description);
+	const [amount, setAmount] = useState(expenseInfo.amount);
+	const [category, setCategory] = useState(categories[expenseInfo.category_id])
+
 	return (
-		<div>
-			This is the expense
+		<div className={styles.outerPage}>
+			<div className={styles.expensesContainer}>
+				<h1>{expenseInfo.name}</h1>
+				<label>Description</label>
+				<input
+					className={styles.input}
+					type="text"
+					value={description}
+					onChange={(e) => setDescription(e.value)}
+				/>
+				<label>Category</label>
+				<input
+					className={styles.inputCategory}
+					list="categories"
+					value={category}
+					onChange={(e) => setCategory(e.value)}
+				/>
+				<datalist id="categories">
+					<option value="Production"></option>
+					<option value="Operation" />
+					<option value="Financial" />
+					<option value="Vendor" />
+					<option value="Manpower" />
+					<option value="Software" />
+					<option value="Hardware" />
+				</datalist>
+				<label>Amount</label>
+				<input
+					className={styles.inputNumber}
+					type="number"
+					value={amount}
+					onChange={(e) => setAmount(e.value)}
+				/>
+				<div className={styles.buttonsContainer}>
+					<input
+						type="submit"
+						className={styles.submitButton}
+					/>
+					<button
+						className={styles.deleteButton}
+					>Delete</button>
+				</div>
+			</div>
 		</div>
 	)
 }
