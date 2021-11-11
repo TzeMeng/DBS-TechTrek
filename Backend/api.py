9a02008f1262c6bc0bcaa8686a4c999989cc0224
Kinfinityoo <93419887+Kinfinityoo@users.noreply.github.com>
2021-11-11 16:18:44 +0800

    Small update to prepare for Tze Ming

--- Backend/api.py
+++ Backend/api.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with, inputs
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,12 +10,15 @@ db = SQLAlchemy(app)
 
 #  Supporting code to add new expense to db
 expense_post_args = reqparse.RequestParser()
+# expense_post_args.add_argument("Id", type=int, help="Expense ID is required", required=True)
+expense_post_args.add_argument("Project_id", type=int, help="Project ID is required", required=True)
+expense_post_args.add_argument("Category_id", type=int, help="Category ID is required", required=True)
 expense_post_args.add_argument("Name", type=str, help="Expense name is required", required=True)
 expense_post_args.add_argument("Description", type=str, help="Expense description is required", required=True)
 expense_post_args.add_argument("Amount", type=int, help="Expense amount is required", required=True)
-expense_post_args.add_argument("Created_at", type=datetime, help="When was this expense created?", required=True)
+expense_post_args.add_argument("Created_at", type=str, help="When was this expense created?", required=True)
 expense_post_args.add_argument("Created_by", type=str, help="Who created this expense?", required=True)
-expense_post_args.add_argument("Updated_at", type=datetime, help="When was this expense updated?", required=True)
+expense_post_args.add_argument("Updated_at", type=str, help="When was this expense updated?", required=True)
 expense_post_args.add_argument("Updated_by", type=str, help="Who updated this expense?", required=True)
 
 # Supporting code to update existing expense in db
@@ -36,9 +39,9 @@ resource_fields = {
 	'Name': fields.String,
     'Description': fields.String,
 	'Amount': fields.Integer,
-    'Created_at': fields.DateTime,
+    'Created_at': fields.String,
     'Created_by': fields.String,
-    'Updated_at': fields.DateTime,
+    'Updated_at': fields.String,
     'Updated_by': fields.String,
 }
 
@@ -79,7 +82,7 @@ class expense(Resource):
 		args = expense_post_args.parse_args()
 		result = expenseModel.query.filter_by(Id=expense_id).first()
 		if result: abort(409, message="Expense ID taken")
-        
+
 		expense = expenseModel(Id=expense_id, Project_id=args['Project_id'], Category_id=args['Category_id'],\
                 Name=args['Name'], Description=args['Description'], Amount=args['Amount'],\
                 Created_at=args['Created_at'], Created_by=args['Created_by'],\
@@ -93,8 +96,7 @@ class expense(Resource):
         #  To modify expense
 		args = expense_update_args.parse_args()
 		result = expenseModel.query.filter_by(Id=expense_id).first()
-		if not result:
-			abort(404, message="Expense doesn't exist")
+		if not result: abort(404, message="Expense doesn't exist")
 
 		if args['Name']: result.Name = args['Name']
         # if args['Description']: result.Description = args['Description']
@@ -112,8 +114,69 @@ class expense(Resource):
 		del expenses[expense_id]
 		return '', 204
 
+#  To add new expense to the db
+class projectModel(db.Model): 
+    Id = db.Column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer, nullable=False)
+    Name = db.Column(db.String(100), nullable=False)
+    Description = db.Column(db.String(100), nullable=False)
+    Budget = db.Column(db.Integer, nullable=False)
+    
+    def __repr__(self):
+        return f'''project(ID = {Id}, User_id = {User_id}, Name = {Name}, \
+            Description = {Description}, Budget = {Budget})''' 
+
+# Main code to api calls (For project related stuffs)
+# Code is able to: query, add, modify, delete
+# get, post, patch, delete
+class project(Resource):
+    
+	@marshal_with(resource_fields)
+	def get(self, project_id):
+        # To query existing project from table
+		result = projectModel.query.filter_by(Id=project_id).first()
+		if not result: abort(404, message="Could not find project with that ID")
+		return result
+
+	@marshal_with(resource_fields)
+	def post(self, project_id):
+        # To add new project
+		args = project_post_args.parse_args()
+		result = projectModel.query.filter_by(Id=project_id).first()
+		if result: abort(409, message="project ID taken")
+        
+		project = projectModel(Id=project_id, User_id=args['User_id'],\
+                Name=args['Name'], Description=args['Description'], Budget=args['Budget'])
+		db.session.add(project)
+		db.session.commit()
+		return project, 201
+
+	@marshal_with(resource_fields)
+	def patch(self, project_id):
+        #  To modify project
+		args = project_update_args.parse_args()
+		result = projectModel.query.filter_by(Id=project_id).first()
+		if not result:
+			abort(404, message="project doesn't exist")
+
+		if args['Name']: result.Name = args['Name']
+        # if args['Description']: result.Description = args['Description']
+		if args['Amount']: result.Amount = args['Amount']
+        # if args['Created_by']: result.Created_by = args['Created_by']
+        # if args['Updated_by']: result.Updated_by = args['Updated_by']
+
+		db.session.commit()
+
+		return result
+
+	def delete(self, project_id):
+        # To delete project
+		abort_if_project_id_doesnt_exist(project_id)
+		del projects[project_id]
+		return '', 204
+
 # To add route to flask for the api call
-# api.add_resource(projects, "/projects/<int:user_id>") 
+api.add_resource(project, "/projects/<int:user_id>") 
 api.add_resource(expense, "/expense/<int:expense_id>") 
 
 if __name__ == "__main__":
--- Backend/api_test.py
+++ Backend/api_test.py
@@ -1,10 +1,12 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-payload = {"Id": 3, "Project_id":3, "Category_id":3, "Name":"TESTn", "Description":"TESTd", "Amount":1, "Created_at": "Sat, 06 Nov 2021 16:00:00", "Created_by":"TESTc", "Updated_at":"Sat, 06 Nov 2021 16:00:00", "Updated_by":"TESTu"}
+payload = {"Id": 3, "Project_id":3, "Category_id":3, "Name":"TESTn", "Description":"TESTd", "Amount":1, "Created_at": "2020-01-06T00:00:00.000Z", "Created_by":"TESTc", "Updated_at":"2020-01-06T00:00:00.000Z", "Updated_by":"TESTu"}
 
 # response = requests.get(BASE + "expense/1")
 response = requests.post(BASE + "expense/3", payload)
 # response = requests.patch(BASE + "expense/2", {})
 # response = requests.delete(BASE + "expense/2", {})
 print(response.json())
+
+
