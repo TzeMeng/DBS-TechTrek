69cefa53978dd8a84ad5e35e632ec3bf30d817a9
dominiclet <dominic.let@hotmail.com>
2021-11-11 14:01:33 +0800

    Create new expense form

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,4 +1,4 @@
-import ProjectCard from "../Components/ProjectCard";
+import ProjectCard from "../components/ProjectCard";
 import useAuth from "../hooks/useAuth";
 import styles from "../styles/Projects.module.css";
 
