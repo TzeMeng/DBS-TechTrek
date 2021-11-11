2c2c5897c9354f4c070951fbbe46370f5c638eb1
Kinfinityoo <93419887+Kinfinityoo@users.noreply.github.com>
2021-11-11 13:27:37 +0800

    Finetuning of expenses API

--- Backend/api.py
+++ Backend/api.py
@@ -8,12 +8,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 db = SQLAlchemy(app)
 
 #  Supporting code to add new expense to db
-expense_put_args = reqparse.RequestParser()
-expense_put_args.add_argument("Name", type=str, help="Expense name is required", required=True)
-expense_put_args.add_argument("Description", type=str, help="Expense description is required", required=True)
-expense_put_args.add_argument("Amount", type=int, help="Expense amount is required", required=True)
-expense_put_args.add_argument("Created_by", type=str, help="Who created this expense?", required=True)
-expense_put_args.add_argument("Updated_by", type=str, help="Who updated this expense?", required=True)
+expense_post_args = reqparse.RequestParser()
+expense_post_args.add_argument("Name", type=str, help="Expense name is required", required=True)
+expense_post_args.add_argument("Description", type=str, help="Expense description is required", required=True)
+expense_post_args.add_argument("Amount", type=int, help="Expense amount is required", required=True)
+expense_post_args.add_argument("Created_by", type=str, help="Who created this expense?", required=True)
+expense_post_args.add_argument("Updated_by", type=str, help="Who updated this expense?", required=True)
 
 # Supporting code to update existing expense in db
 expense_update_args = reqparse.RequestParser()
@@ -24,10 +24,10 @@ expense_update_args.add_argument("Created_by", type=str, help="Who created this 
 expense_update_args.add_argument("Updated_by", type=str, help="Who updated this expense?", required=True)
 
 # Supporting code to transform data and serialise it before being processed
-    # 'Id': fields.Integer,
-    # 'Project_id': fields.Integer,
-    # 'Category_id': fields.Integer,
 resource_fields = {
+    'Id': fields.Integer,
+    'Project_id': fields.Integer,
+    'Category_id': fields.Integer,
 	'Name': fields.String,
     'Description': fields.String,
 	'Amount': fields.Integer,
@@ -62,13 +62,15 @@ class expense(Resource):
 		return result
 
 	@marshal_with(resource_fields)
-	def put(self, expense_id):
+	def post(self, expense_id):
         # To add new expense
-		args = expense_put_args.parse_args()
+		args = expense_post_args.parse_args()
 		result = expenseModel.query.filter_by(Id=expense_id).first()
 		if result: abort(409, message="Expense ID taken")
-
-		expense = expenseModel(Id=expense_id, Name=args['Name'], Amount=args['Amount'])
+        
+		expense = expenseModel(Id=expense_id, Project_id=args['Project_id'], Category_id=args['Category_id'],\
+                Name=args['Name'], Description=args['Description'], Amount=args['Amount'],\
+                Created_by=args['Created_by'], Updated_by=args['Updated_by'])
 		db.session.add(expense)
 		db.session.commit()
 		return expense, 201
@@ -82,7 +84,10 @@ class expense(Resource):
 			abort(404, message="Expense doesn't exist")
 
 		if args['Name']: result.Name = args['Name']
+        # if args['Description']: result.Description = args['Description']
 		if args['Amount']: result.Amount = args['Amount']
+        # if args['Created_by']: result.Created_by = args['Created_by']
+        # if args['Updated_by']: result.Updated_by = args['Updated_by']
 
 		db.session.commit()
 
@@ -95,7 +100,7 @@ class expense(Resource):
 		return '', 204
 
 # To add route to flask for the api call
-# api.add_resource(projects, "/projects/<int:project_id>") 
+# api.add_resource(projects, "/projects/<int:user_id>") 
 api.add_resource(expense, "/Id/<int:expense_id>") 
 
 if __name__ == "__main__":
--- Backend/api_test.py
+++ Backend/api_test.py
@@ -1,7 +1,8 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
+payload = {"Id": 1, "Project_id":1, "Category_id":1, "Name":"TESTn", "Description":"TESTd", "Amount":1, "Created_by":"TESTc", "Updated_by":"TESTu"}
 
 # response = requests.patch(BASE + "expense/2", {})
-response = requests.put(BASE + "expense/2", {})
+response = requests.post(BASE + "expense/1", payload)
 print(response.json())
